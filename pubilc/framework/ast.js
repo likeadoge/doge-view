25ceaca1ad3416a7c8046209364262e05095ba1c
like_a_doge <like_a_doge@outlook.com>
2020-10-07 02:12:46 +0800

    Update ast.js

--- pubilc/framework/ast.js
+++ pubilc/framework/ast.js
@@ -18,82 +18,86 @@ import {
 
 export class AstNode {
     scope = null
-    children = []
 
-    constructor(target, globe, parent) {
-        this.scope = parent ? Scope.extend(parent.scope) : globe
+    constructor(parent) {
+        this.scope = Scope.extend(parent)
     }
 
-    toHtml(data){return ''}
+    toHtml(data) { return '' }
+}
+
+export class AstParentNode extends AstNode {
+    children = []
+    toHtml(data) { return this.children.map(v => v.toHtml(data)).join('') }
+}
+
+export class AstHtmlNode extends AstNode {
+    content = ''
+    constructor(scope, content) {
+        super(scope)
+        this.content = content
+    }
+    toHtml(data) { return this.content }
 }
 
 export class AstTextNode extends AstNode {
-    content = ''
-    constructor(target, globe, parent) {
-        super(target, globe, parent)
-        this.content = target.content
-    }
-    toHtml(data){return this.content}
-}
-
-export class AstCodeNode extends AstNode {
     #code = ''
     #fn = null
-    constructor(target, globe, parent) {
-        super(target, globe, parent)
-        this.#code = target.data.code
-        this.#fn = new Func(this.scope,this.#code)
+    constructor(scope, code) {
+        super(scope)
+        this.#code = code
+        this.#fn = new Func(this.scope, this.#code)
     }
-    toHtml(data){return this.#fn.apply(data)}
+    toHtml(data) { return this.#fn.apply(data) }
 }
 
-export class AstIfNode extends AstNode {
+export class AstIfNode extends AstParentNode {
     else = null
     #code = ''
     #fn = null
-    constructor(target, globe, parent) {
-        super(target, globe, parent)
-        this.#code = target.data.code
-        this.#fn = new Func(this.scope,this.#code)
+    constructor(parent, code) {
+        super(parent)
+        this.#code = code
+        this.#fn = new Func(this.scope, this.#code)
     }
 
-    toHtml(data){
+    toHtml(data) {
         const s = this.#fn.apply(data)
-        if(s){
-            return this.children.map(v=>v.toHtml(data)).join('')
-        }else if(this.else){
+        if (s) {
+            return this.children.map(v => v.toHtml(data)).join('')
+        } else if (this.else) {
             return this.else.toHtml(data)
-        }else{
+        } else {
             return ''
         }
     }
 
 }
 
-export class AstLoopNode extends AstNode {
+export class AstLoopNode extends AstParentNode {
     #valueField = ''
     #indexField = ''
     #code = ''
     #fn = null
 
-    constructor(target, globe, parent) {
-        super(target, globe, parent)
-        this.#code = target.data.code
-        this.#valueField = target.data.input[0] || null
-        this.#indexField = target.data.input[1] || null
-        this.scope = Scope.extend(parent.scope, [
+    constructor(parent, code, valueField = null, indexField = null) {
+        super(parent)
+        this.#code = code
+        this.#valueField = valueField
+        this.#indexField = indexField
+        this.scope = Scope.extend(parent, [
             this.#valueField, this.#indexField
-        ].filter(v=>v))
-        this.#fn = new Func(this.scope,this.#code)
+        ].filter(v => v))
+        this.#fn = new Func(this.scope, this.#code)
     }
-    toHtml(data){
+    toHtml(data) {
         const s = this.#fn.apply(data)
-        return s.map((value,index)=>{
-            return this.children.map(v=>v.toHtml(Object.assign(
+        return s.map((value, index) => {
+            return this.children.map(v => v.toHtml(Object.assign(
                 {},
                 data,
-                this.#valueField?{[this.#valueField]:value}:null,
-                this.#indexField?{[this.#indexField]:index}:null
+                this.#valueField ? { [this.#valueField]: value } : null,
+                this.#indexField ? { [this.#indexField]: index } : null
             )))
         }).flat().join('')
     }
@@ -102,16 +106,15 @@ export class AstLoopNode extends AstNode {
 
 export class AstProgramNode extends AstNode {
     children = []
-    constructor(target, globe, parent) {
-        super(target, globe, parent)
-        this.scope = globe
+    constructor(globe) {
+        super(globe)
     }
-    toHtml(data){
-        return this.children.map(v=>v.toHtml(data)).join('')
+    toHtml(data) {
+        return this.children.map(v => v.toHtml(data)).join('')
     }
 }
 
-export function toAstNodeList(current, globe, parent = null) {
+function dealParserNode(current, parent = null) {
 
     if (!current) {
         return []
@@ -119,44 +122,44 @@ export function toAstNodeList(current, globe, parent = null) {
 
     if (current instanceof ParserProgarmNode) {
         const [node] = current.children
-        const astNode = new AstProgramNode(current, globe, parent)
-        astNode.children = toAstNodeList(node, globe, astNode)
+        const astNode = new AstProgramNode(parent)
+        astNode.children = dealParserNode(node, astNode.scope)
         return [astNode]
     }
 
     if (current instanceof ParserExprListNode) {
         const [expr, exprList] = current.children
-        return [...toAstNodeList(expr, globe, parent), ...toAstNodeList(exprList, globe, parent)]
+        return [...dealParserNode(expr, parent), ...dealParserNode(exprList, parent)]
     }
 
     if (current instanceof ParserExprNode) {
         const [node, ...extra] = current.children
 
         if (node instanceof LexerTextNode) {
-            const astNode = new AstTextNode(node, globe, parent)
+            const astNode = new AstHtmlNode(parent, node.content)
             return [astNode]
         }
 
         if (node instanceof LexerCodeNode) {
-            const astNode = new AstCodeNode(node, globe, parent)
+            const astNode = new AstTextNode(parent, node.data.code)
             return [astNode]
         }
 
         if (node instanceof LexerLoopNode) {
-            const astNode = new AstLoopNode(node, globe, parent)
+            const astNode = new AstLoopNode(parent, node.data.code, node.data.input[0], node.data.input[1])
             const [exprList, over] = extra
             if (!over instanceof LexerOverNode) {
                 throw new Error('ast error!!!')
             }
-            astNode.children = toAstNodeList(exprList, globe, astNode)
+            astNode.children = dealParserNode(exprList, astNode.scope)
             return [astNode]
         }
 
         if (node instanceof LexerIfNode) {
-            const astNode = new AstIfNode(node, globe, parent)
+            const astNode = new AstIfNode(parent, node.data.code)
             const [exprList, tail] = extra
-            astNode.children = toAstNodeList(exprList, globe, astNode)
-            astNode.else = toAstNodeList(tail, globe, parent)[0] || null
+            astNode.children = dealParserNode(exprList,astNode.scope)
+            astNode.else = dealParserNode(tail, parent)[0] || null
             return [astNode]
         }
     }
@@ -167,10 +170,10 @@ export function toAstNodeList(current, globe, parent = null) {
             return []
         }
         if (node instanceof LexerElseNode) {
-            const astNode = new AstIfNode(node, globe, parent)
+            const astNode = new AstIfNode(parent, node.data.code)
             const [exprList, tail] = extra
-            astNode.children = toAstNodeList(exprList, globe, astNode)
-            astNode.else = toAstNodeList(tail, globe, parent)[0] || null
+            astNode.children = dealParserNode(exprList, astNode.scope)
+            astNode.else = dealParserNode(tail, parent)[0] || null
             return [astNode]
         }
     }
@@ -181,7 +184,7 @@ export function toAstNodeList(current, globe, parent = null) {
 
 
 export function toAst(parserNode, globeData) {
-    return toAstNodeList(parserNode, new Scope(Object.keys(globeData)))[0]
+    return dealParserNode(parserNode, new Scope(Object.keys(globeData)))[0]
 }
 
 
